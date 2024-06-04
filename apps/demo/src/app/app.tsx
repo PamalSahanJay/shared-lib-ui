a7234db3a61a977d3ea181c8fcfa2f2aa158e513
psahan <psahan@wiley.com>
2024-06-04 08:26:09 +0530

    git add .

--- apps/demo/src/app/app.tsx
+++ apps/demo/src/app/app.tsx
@@ -1,12 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NxWelcome from './nx-welcome';
+
+import {Authentication} from '@wiley/shared-ui/authentication'
+import {CommonUi} from '@wiley/shared-ui/common-ui'
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="demo" />
+      {/* <NxWelcome title="demo" /> */}
+      <BrowserRouter> 
+        <Routes>
+          <Route path="/auth" element={<Authentication />} />
+          <Route path="/pamal" element={ <div>pamal</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
